8433cce241aba4bea0a58d100cdcbe2fc9c850e3
Francesco <rassegnamoci@tavolarotonda.org>
2024-05-12 23:33:29 +0200

    Barriere

--- helpers/movieslist_generator.py
+++ helpers/movieslist_generator.py
@@ -9,7 +9,8 @@ from jinja2 import Template
 pd.set_option('display.max_colwidth', None)
 
 df = pd.read_csv('../rawdata/movieslist.csv')
-df = df[['Data proiezione', 'Film',	'Selezionatore', 'Tema', 'Anno', 'Regista/i', 'Durata',	'Paese/i di produzione', 'Link IMDb']]
+df = df[['Data proiezione', 'Film',	'Selezionatore', 'Tema', 'Anno', \
+         'Regista/i', 'Durata', 'Paese/i di produzione', 'Link IMDb']]
 
 # Sort by last movie
 df.set_index(['Data proiezione'])
@@ -18,7 +19,9 @@ df = df.sort_index(ascending = False)
 with open('../templates/movieslist_template.html','r') as f:
     template = f.read()
 
-data = {'table': df.to_html(index = False, formatters={'Link IMDb': lambda x: '<a href="'+x+'" target="_blank">'+x+'</a>'}).replace('&lt;','<').replace('&gt;','>')}
+data = {'table': df.to_html(index = False, formatters={'Link IMDb': lambda x: \
+        '<a href="'+x+'" target="_blank">'+x+'</a>'}).replace('&lt;', \
+        '<').replace('&gt;','>')}
 
 j2_template = Template(template)
 
